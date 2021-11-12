6c72d2d96666da9bc46abcf9bae14bf06acf1688
humtej1204 <90359858+humtej1204@users.noreply.github.com>
2021-11-12 15:02:37 -0500

    Writing the code

---
+++ 0x00-math_complex/3-argument.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * argument - function that returns the argument of a given complex number
+ *
+ * @c: Parameter structure
+ *
+ * Return: returns the argument of a given complex number
+ */
+double argument(complex c)
+{
+        double x, ret;
+        
+        x = c.im / c.re;
+        ret = atan (x);
+
+        return (ret);
+}
