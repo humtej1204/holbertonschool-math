4fdda4b95f957bfd8a88785b1bf2268315eebd5c
humtej1204 <90359858+humtej1204@users.noreply.github.com>
2021-11-12 13:21:24 -0500

    Adding more code

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -16,11 +16,31 @@ void display_complex_number(complex c)
 	}
 	else if (c.re == 0)
 	{
+		if (c.im == 1)
+		{
+			printf("i\n");
+			return;
+		}
+		else if (c.im == -1)
+		{
+			printf("-i\n");
+			return;
+		}
 		printf("%.9gi\n", c.im);
 	}
 	else
 	{
 		c.im = (c.im < 0) ? -(c.im) : c.im;
+		if (c.im == 1)
+		{
+			printf("%.9g %c i\n", c.re, x);
+			return;
+		}
+		else if (c.im == -1)
+		{
+			printf("%.9g %c -i\n", c.re, x);
+			return;
+		}
 		printf("%.9g %c %.9gi\n", c.re, x, c.im);
 	}
 }
