1ff7b86c09466d2a6d5830f6fb882ad9b1dd1fb4
humtej1204 <humtej1204@gmail.com>
2021-11-12 15:44:50 -0500

    Correcting code

--- 0x00-math_complex/6-multiplication.c
+++ 0x00-math_complex/6-multiplication.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
- * multiplication - function that performs the multiplication operation to
- * complex numbers
+ * multiplication - function that performs the multiplication
+ * operation to complex numbers
  *
  * @c1: Parameter structure 1
  * @c2: Parameter structure 2
@@ -9,11 +9,11 @@
  */
 void multiplication(complex c1, complex c2, complex *c3)
 {
-        double m_arg, m_mod;
+	double m_arg, m_mod;
 
-        m_arg = modulus(c1) * modulus(c2);
-        m_mod = argument(c1) + argument(c2);
+	m_arg = modulus(c1) * modulus(c2);
+	m_mod = argument(c1) + argument(c2);
 
-        c3->re = m_arg * cos(m_mod);
-        c3->im = m_arg * sin(m_mod);
+	c3->re = m_arg * cos(m_mod);
+	c3->im = m_arg * sin(m_mod);
 }
