5fb4b11c52c06e17f4c529687f1e4ee2623227f9
humtej1204 <90359858+humtej1204@users.noreply.github.com>
2021-11-12 15:24:28 -0500

    Writing the code

---
+++ 0x00-math_complex/5-substraction.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * addition - function that performs the substraction operation to complex numbers
+ *
+ * @c1: Parameter structure 1
+ * @c2: Parameter structure 2
+ * @c3: Parameter pointer to structure
+ *
+ * Return: return the rest
+ */
+void substraction(complex c1, complex c2, complex *c3)
+{
+        c3->re = c1.re - c2.re;
+        c3->im = c1.im - c2.im;
+}
