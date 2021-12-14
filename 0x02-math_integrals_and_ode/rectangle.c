c0493dc197e07c50c1942d1d631ae62ea4f702e9
Humberto Tejada <humtej1204@gmail.com>
2021-12-14 04:25:01 -0500

    Correcting Betty Style

--- 0x02-math_integrals_and_ode/rectangle.c
+++ 0x02-math_integrals_and_ode/rectangle.c
@@ -13,10 +13,22 @@ double rectangle_method(double a, double b, int steps)
 {
 	double var_x = (b - a) / steps,
 	f_x,
+	/* mp_x = (a + a + var_x) / 2, */
 	lp_x = (a) / 2,
+	/* rp_x = (a + var_x) / 2, */
 	res = 0;
 	int i = 0;
 
+/**	Aprox. with Middle Point
+	while(i < steps)
+	{
+		f_x = var_x * (1/(1 + (mp_x * mp_x)));
+		res = res + f_x;
+		mp_x = mp_x + var_x;
+		printf("%d\n", i);
+		i++;
+	}
+*/
 	/* Aprox. with Left Point */
 	while (i < steps)
 	{
@@ -26,6 +38,16 @@ double rectangle_method(double a, double b, int steps)
 		printf("%d\n", i);
 		i++;
 	}
+/**	Aprox. with Right Point
+	while(i < steps)
+	{
+		f_x = var_x * (1/(1 + (rp_x * rp_x)));
+		res = res + f_x;
+		rp_x = rp_x + var_x;
+		printf("%d\n", i);
+		i++;
+	}
+ */
 
 	return (res);
 }
